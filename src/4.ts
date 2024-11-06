67017b21c2fe30837c6a2c3373f5d06f676a89d9
Sv-L <asl.warmhaus@gmail.com>
2024-11-06 10:01:02 +0100

    1

--- src/4.ts
+++ src/4.ts
@@ -1,10 +1,66 @@
+class Key{
+    private signature: number;
+    constructor() {
+        this.signature = Math.random()
+    }
+
+    getSignature(): number{
+        return this.signature
+    }
+}
+
+class Person {
+    private key: Key
+
+    constructor(key: Key) {
+        this.key = key
+        console.log('key Person', key)
+    }
+
+    getKey():Key{
+        return this.key
+    }
+}
+
+
+abstract class House{
+    protected tenants: Person[] = [];
+    protected door: boolean = false;
+    protected key: Key
+
+    constructor(key: Key) {
+        this.key = key
+    }
+
+     public abstract openDoor(key: Key): void
+
+    public comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person)
+            console.log("Person has entered the house.")
+        } else {
+            console.log("The door is closed. Can't enter.")
+        }
+    }
+   
+}
+
+class MyHouse extends House {
+    public openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log("Door is opened.")
+        } else {
+            console.log("Wrong key! The door remains closed.")
+        }
+    }
+}
+
 const key = new Key();
-
-const house = new MyHouse(key);
+console.log(key.getSignature())
 const person = new Person(key);
-
+const house = new MyHouse(key);
 house.openDoor(person.getKey());
-
 house.comeIn(person);
 
 
